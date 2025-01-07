c43703433fab7aec7b95947477e9d49a8a7b23f2
Onyx Devs Lab <github@onyxdevslab.com>
2025-01-07 17:43:10 -0500

    reeee

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/onyx-devs-lab.github.io/", // Replace with your repo name
+  base: "/", // Replace with your repo name
   build: {
     outDir: "dist",
     assetsDir: "assets",
